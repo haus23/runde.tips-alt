c808d278a090afe4bc4fe8847762875a6c8c48d5
Micha Alt <micha@haus23.net>
2021-10-30 00:10:18 +0200

    Turn site into app ;-)

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+console.log('Hooray, runde.tips is starting');
+
+const rootElt = document.createElement('h1');
+rootElt.textContent = 'runde.tips';
+
+document.getElementById('root').appendChild(rootElt);
