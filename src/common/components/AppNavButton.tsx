03bd5ee132b4234ffa80eb66dee61fa9843fe129
Micha Alt <micha@haus23.net>
2021-11-03 21:37:14 +0100

    Change accent color.

--- src/common/components/AppNavButton.tsx
+++ src/common/components/AppNavButton.tsx
@@ -10,7 +10,7 @@ export default function AppNavButton({ onClick, children }: AppNavButtonProps) {
     <button
       type="button"
       className="p-1 rounded-md text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2
-        bg-white hover:bg-gray-100  hover:text-gray-500 focus:ring-gray-400
+        bg-white hover:bg-gray-100  hover:text-gray-500 focus:ring-indigo-500
         dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white  dark:focus:ring-offset-gray-800 dark:focus:ring-white
       "
       onClick={onClick}
