6deb567fc9eb311526f0de000df8b70f76ee8b66
Micha Alt <micha@haus23.net>
2021-11-03 10:15:08 +0100

    Provide recoil root.

--- src/common/context/index.tsx
+++ src/common/context/index.tsx
@@ -1,10 +1,15 @@
 import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 type AppProviderProps = {
   children: ReactNode;
 };
 
 export default function AppProvider({ children }: AppProviderProps) {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <RecoilRoot>
+      <BrowserRouter>{children}</BrowserRouter>
+    </RecoilRoot>
+  );
 }
