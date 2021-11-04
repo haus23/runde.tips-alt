6c482d60ffb5eec82df2fdbb6912fb68b9bde65d
Micha Alt <micha@haus23.net>
2021-11-04 15:14:19 +0100

    Adjust styles.

--- src/common/components/AppNavButton.tsx
+++ src/common/components/AppNavButton.tsx
@@ -9,7 +9,7 @@ export default function AppNavButton({ onClick, children }: AppNavButtonProps) {
   return (
     <button
       type="button"
-      className="p-1 rounded-md text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2
+      className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2
         bg-white hover:bg-gray-100  hover:text-gray-500 focus:ring-indigo-500
         dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white  dark:focus:ring-offset-gray-800 dark:focus:ring-white
       "
