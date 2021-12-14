fa0ee7060bf3c6332e0e69c3cbf07072ad4c1553
Micha Alt <micha@haus23.net>
2021-12-14 13:30:02 +0100

    Redirect if already authenticated.

--- src/front-of-house/views/LogIn.tsx
+++ src/front-of-house/views/LogIn.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import useAuth from '@/common/hooks/use-auth';
+
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import auth from '@/api/firebase/auth';
 
 const LogIn = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { authenticated } = useAuth();
+
+  const from = location.state?.from?.pathname || '/';
+
+  useEffect(() => {
+    if (authenticated) {
+      navigate(from, { replace: true });
+    }
+  }, [authenticated]);
+
   const logIn = async () => {
     await signInWithEmailAndPassword(
       auth,
