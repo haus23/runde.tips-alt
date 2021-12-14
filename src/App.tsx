5b119a670a88928f3263988e1cc8d8dc9e75d16f
Micha Alt <micha@haus23.net>
2021-12-14 13:30:34 +0100

    Code splitting for backyard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from '@/front-of-house/Layout';
 import Standings from '@/front-of-house/views/Standings';
 import LogIn from '@/front-of-house/views/LogIn';
-import BackyardApp from '@/BackyardApp';
 import RequireAuth from '@/common/components/router/RequireAuth';
+
+const BackyardApp = lazy(() => import('./BackyardApp'));
 
 function App() {
   return (
